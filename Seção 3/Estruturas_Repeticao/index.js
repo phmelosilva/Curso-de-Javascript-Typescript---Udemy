45de3eff404be2149c5dac173f60fa6c4bde545b
Pedro Henrique <pedrodsm1819@gmai.com>
2022-10-05 19:55:58 -0300

    Estudando estrutura de repetição for

---
+++ Seção 3/Estruturas_Repeticao/index.js
@@ -0,0 +1,15 @@
+// i - index
+for (let i = 0; i <= 10; i++ ) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+
+
+
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Laranja', 'Maracujá'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
+ 
