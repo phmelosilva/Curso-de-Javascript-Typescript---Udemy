1af18faca8c7e393b7f54e9df01470e602d1ddf9
Pedro Henrique <pedrodsm1819@gmai.com>
2022-10-14 18:11:32 -0300

    Estudando estrutura Finally no tratamento de Erros

---
+++ Seção 3/Tratamentos_de_Erros/Try_Catch_Finally.js
@@ -0,0 +1,58 @@
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) { //quando a variável não é uma instância
+        throw new TypeError('Esperando instância de Date.');
+    }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit', 
+        second: '2-digit',   
+        hour12: false
+    });
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora(data);
+    console.log(hora);
+} catch(e) {
+    // Tratar erro
+    //console.log(e);
+} finally {
+    console.log('Tenh um bom dia.');
+}
+
+
+
+
+
+
+
+
+
+// try {
+  
+//     console.log('Abri um arquivo'); // É executada quando não há erros
+//     console.log('Maipulei o arquivo e gerou erro');
+//     console.log('Fechei o arquivo');    
+    
+//     // Este outro try catch também pode ser executado!
+//     try {
+//         console.log(b);
+//     } catch(e) {
+//         console.log('Deu erro');
+//     } finally {
+//         console.log('Também sou finally.');
+//     }
+    
+// } catch (e) {
+//     console.log('Tratando o erro');  //Não fecharia o arquivo, só trataria o erro
+
+// } finally {
+//     console.log('FINALLY: Eu sempre sou executado'); // É executado sempre, mas podemos omiti-lo
+
+// }
